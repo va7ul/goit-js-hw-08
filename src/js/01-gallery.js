f02034e2a85f5033b452c1fcedf24653c4f93820
va7ul <vas7ul@gmail.com>
2023-06-19 13:53:32 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.style.listStyle = 'none';
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+function createGalleryMarkup(items) {
+    return items.map(({preview, original, description}) => 
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`)
+    .join('');
+};
+
+imageFullscreenOpener();
+
+function imageFullscreenOpener() {
+    new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
 
 console.log(galleryItems);
