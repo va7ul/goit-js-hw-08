3557b613f3280afa666a26559a82100d39dda1d9
va7ul <vas7ul@gmail.com>
2023-06-19 18:05:04 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const saveTime = function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);    
+};
+
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+
+const getCurrentTime = function (event) {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    return currentTime;
+};
+
+
+player.setCurrentTime(getCurrentTime()).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
